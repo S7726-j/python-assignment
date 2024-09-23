dbb8967abc2777619d0f3bf1c7c7ead4b57e623d
S7726-j <souravjdas7726@gmail.com>
2024-09-23 23:34:23 +0530

    Update students_dictionary.py

--- students_dictionary.py
+++ students_dictionary.py
@@ -1,1 +1,29 @@
 
+
+students = []
+
+
+def add_student(student_id, student_name, student_grade):
+    
+    for student in students:
+        if student['ID'] == student_id:
+            print(f"Student ID {student_id} already exists.")
+            return
+    
+    
+    new_student = {
+        'ID': student_id,
+        'Name': student_name,
+        'Grade': student_grade
+    }
+    students.append(new_student)
+    print(f" {student_name} added successfully.")
+
+
+add_student(101, 'Akash', 'A')
+add_student(102, 'Madhab', 'B')
+add_student(101, 'Abhinash', 'A')  
+add_student(104, 'Abhimanyu', 'A+')
+
+
+print(students)
