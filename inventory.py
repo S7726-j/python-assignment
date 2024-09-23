8579bb6f0274b7a4e94c084961919b14db44d18a
S7726-j <souravjdas7726@gmail.com>
2024-09-23 23:32:54 +0530

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,1 +1,26 @@
+inventory = {}
+
+def add_inventory(item , price , quantity):
+    inventory[item] = ( "₹" + str(price), quantity )
+
+def update(item_update , sale_quantity, quantity):
+    inventory[item_update] = ( "₹" + str(price), quantity - sale_quantity )
+    
+    
+
+n = int(input("Enter no. of items : "))
+
+for i in range(n):
+    item = input(" Enter item : ")
+    price = int(input(" Enter price : "))
+    quant = int(input(" Enter quantity : "))
+    print()
+    add_inventory(item, price, quant)
+    print(f"Inventory : {inventory1}")
+    print()
+    
+item_update = input("Enter item to update quantity : ")
+sale_quant = int(input(" Enter sale quantity : "))
+update(item_update, sale_quant, quant)
+print(inventory)
 
